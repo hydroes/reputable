d84653fa77587f8d6a84b8c44d1339a67faea3e0
unknown <bgouws@wgbgouws.office.webgains.com>
2013-10-03 16:25:23 +0100

    Added twitter API credentials

---
+++ app/config/twitter.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Twitter API credentials
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+	'consumer_key' => 'h54qHRatZK1EphoX4MQ8AA',
+	
+	'consumer_secret' => 'KY5R6EhIMMjT4Uew5hOwauUySV0d05vyzvhBY58EA',
+	
+	'request_token' => 'https://api.twitter.com/oauth/request_token',
+	
+	'authorize_url' => 'https://api.twitter.com/oauth/authorize',
+	
+	'access_token_url' => 'https://api.twitter.com/oauth/access_token',
+	
+	'callback_url' => 'http://reputable.eu01.aws.af.cm/welcome',
+	
+);
